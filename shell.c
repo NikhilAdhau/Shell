8f530858a718bc63968afbf353ba1add3a44bd97
Nikhil Adhau <nikhiladhau999@gmail.com>
2021-01-30 15:09:17 +0530

    pipe version 1

--- shell.c
+++ shell.c
@@ -6,8 +6,17 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <wait.h>
+#include <errno.h>
 
 #define SIZE 1024
+
+//global variables
+int cmd_flag = 0;
+
+typedef struct redirect {
+	int fd;
+	char *filename;
+}redirect;
 
 char* readline() {
 	char c;
@@ -21,13 +30,32 @@ char* readline() {
 		}
 		buffer[pos++] = c;
 	}
-	if (n == 0)
-		write(1, "\n", 1);
+	//if (n == 0)
+	//	write(1, "\n", 1);
 
 	return buffer;
 }
 
-char** parse(char *line) {
+char** parse_pipe(char *line) {
+	char **commands = (char **) malloc(sizeof(char *) * 10);	
+	char *delim = "|", *token;
+	int i = 0;
+	
+	token = strtok(line, delim);
+	if (!token) {
+		commands[i++] = line;
+	}
+	while (token) {
+		cmd_flag++;
+		commands[i++] = token;
+		token = strtok(NULL, delim);
+	}
+	commands[i] = NULL;
+	return commands;
+}
+
+
+char** parse_args(char *line) {
 	char *delim = " \t", *token;
 	char **args = (char **) malloc( sizeof(char* ) * 100 );
 	int i = 0;
@@ -44,28 +72,145 @@ char** parse(char *line) {
 
 }
 
-int main () {
-	char *buf, **args;
-		
-	while (1) {
-		int len;
-		write(1, "> ", 2);
-		buf = readline();
-		args = parse(buf);
-		int pid = fork();
-		if (pid == 0) {  	//child process
-			execve(buf, args, NULL);
+int create_process (char *command) {
+	char **args = parse_args(command);
+	int pid = fork();
+	if (pid == 0) { 	//child process
+		if ( execvp(args[0], args) == -1 ) {
+			perror("exec failed"); 		//if the command not found
 		}
-		else if (pid > 0) { 	//parent process
-			wait(NULL);
-		}
-		else{
-			printf ("Error in fork!\n");
-		}
-		free(buf);
-		free(args);
-	}	
 
-	return 0;
+	}
+	else if (pid > 0) { 	//parent process
+		wait(NULL);
+	}
+	else{
+		perror("fork failed");
+	}
+	free(args);
+	return 0;	
 }
 
+int create_pipe(char **commands) {
+	int i, pid, nopipes;
+	int pipefd[100];
+	char **args;
+	for (i = 0; i < cmd_flag; i++) {
+		args = parse_args(commands[i]);
+		if (i != cmd_flag -1) { 
+			pipe(&pipefd[2 * i]);
+			printf ("pipefd %d %d\n", pipefd[2*i], pipefd[2*i+1]);
+		}
+		pid = fork();
+		if (pid == 0) {
+
+			//if not the first command, open pipe for reading
+			if (i != 0) {
+				//old read fd to 0
+				if (dup2(pipefd[(2 * i) - 2], 0) < 0) {
+					perror ("dup2 failed");
+					break;
+				}
+			}
+
+			//if not the last command, open pipe for writing
+			if (i != cmd_flag - 1) {
+				if (dup2(pipefd[(2 * i )+ 1], 1) < 0) {
+					perror ("dup2 failed");
+					break;
+				}
+
+			}
+
+			//close all pipefds
+			for (nopipes = 0; nopipes <= ((2 * i) + 1); nopipes++) {
+				if (i == cmd_flag - 1 && nopipes == 2 * i)
+					break;	
+				close(pipefd[nopipes]);
+			}
+
+			//exec the new command
+			if (execvp(args[0], args) == -1 ) {
+				perror ("exec failed");
+				break;
+			}
+	
+
+		}
+
+		else if (pid > 0){
+			//if (i == cmd_flag - 1) {
+			//	for (nopipes = 0; nopipes < cmd_flag; nopipes++) {
+			//		wait(NULL);
+			//		printf ("%d child\n", nopipes);
+			//	}
+			//	//wait(NULL);
+			//}
+			//
+			continue;
+		}
+		else {
+			perror ("fork failed");
+		}
+	}
+
+	//while (wait(NULL) > 0);
+
+	for (nopipes = 0; nopipes <= ((2 * i) - 3); nopipes++)
+		close(pipefd[nopipes]);
+	return 0;
+
+}
+
+
+int main () {
+	char *buf, **commands;
+	int i;
+		
+	while (1) {
+		printf("\n");
+		write(1, "prompt> ", strlen("prompt> "));
+		buf = readline();
+		cmd_flag = 0;
+		commands = parse_pipe(buf);
+		if (cmd_flag == 1) {
+			i = create_process(commands[0]);
+		}
+		else {
+			i = create_pipe(commands);
+			//free(commands);
+			//free(buf);
+			//break;
+		}
+		//for (i = 0; i < nocmd; i++) {
+		//	if (cmd_flag - 1) {
+		//		printf ("%d\n", nocmd);
+		//		//pipe(&pipefd[2 * nocmd]);
+		//	}
+		//	else {
+		//		cmd_flag--;
+		//		create_process(commands[i]);	
+		//	}
+		//}
+		free(commands);
+		free(buf);
+		//args = parse_args();
+		//int pid = fork();
+		//if (pid == 0) { 	//child process
+		//	if ( execvp(args[0], args) == -1 ) {
+		//		perror("exec failed"); 		//if the command not found
+		//	}
+
+		//}
+		//else if (pid > 0) { 	//parent process
+		//	wait(NULL);
+		//}
+		//else{
+		//	perror("fork failed");
+		//}
+		//free(buf);
+		//free(args);
+		//free(commands);
+	}	
+	return 0;
+}
